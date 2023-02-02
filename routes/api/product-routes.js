42ec430430becc109175baa1637c98dd13afcb8f
Sam Jensen <samual.jensen@gmail.com>
2023-02-01 17:55:10 -0800

    added get for product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, attributes: ['tag_name'] }],
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    });
+    res.status(200).json(productData);
+  }catch (err){
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // get one product
